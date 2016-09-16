9eb995f321ed9204d22082a6a19e6c184156ed70
housw <housw2010@gmail.com>
2016-09-16 15:24:05 +0200

    modified generate_ACT_comparison_crunch.sh to allow tblastx comparison

--- generate_ACT_comparison_crunch.sh
+++ generate_ACT_comparison_crunch.sh
@@ -2,9 +2,9 @@
 
 # follow procedures in supplementary data of this paper: http://www.nature.com/nprot/journal/v7/n7/full/nprot.2012.068.html
 
-if [ $# -ne 3 ]; then 
+if [ $# -ne 4 ]; then 
     echo ""
-    echo "Usage: $0 [input_first_genome.fna] [input_second_genome.fnaa] [output_filename]"
+    echo "Usage: $0 [blastn/tblastx] [input_first_genome.fna] [input_second_genome.fna] [output_filename]"
     echo ""
     exit 1
 fi
@@ -15,7 +15,7 @@ fi
 #blastall="/vol/biotools/bin/blastall"
 
 # run formatdb
-formatdb -p F -i $1 
+formatdb -p F -i $2 
 
 # do blastall using 30 cores
-blastall -p blastn -m 8 -e 1e-5 -d $1 -i $2 -o $3 -a 30
+blastall -p $1 -m 8 -e 1e-5 -d $2 -i $3 -o $4 -a 30
